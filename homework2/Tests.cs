ca8a923b5346ec2de10f154001b9e67b5b09ec10
marina-popova11 <181482786+marina-popova11@users.noreply.github.com>
2025-02-27 22:25:01 +0300

    Added tests

---
+++ homework2/Tests.cs
@@ -0,0 +1,81 @@
+using PrefixTrie;
+
+/// <summary>
+/// Tests by trie.
+/// </summary>
+public class Tests
+{
+      /// <summary>
+      /// Tests.
+      /// </summary>
+      /// <returns>If all tests passed returns true.</returns>
+      public static bool AllTests()
+      {
+            if (!AddTest())
+            {
+                  System.Console.WriteLine("AddTest is failed.");
+                  return false;
+            }
+
+            if (!ContainsTest())
+            {
+                  System.Console.WriteLine("ContainsTest is failed.");
+                  return false;
+            }
+
+            if (!RemoveTest())
+            {
+                  System.Console.WriteLine("RemoveTest is failed.");
+                  return false;
+            }
+
+            if (!HowManyStartsWithPrefixTest())
+            {
+                  System.Console.WriteLine("HowManyStartsWithPrefixTest is failed.");
+                  return false;
+            }
+
+            return true;
+      }
+
+      private static bool AddTest()
+      {
+            Trie trie = new();
+            trie.AddElement("dog");
+            trie.AddElement("doing");
+            return trie.Size == 2;
+      }
+
+      private static bool ContainsTest()
+      {
+            Trie trie = new();
+            trie.AddElement("dog");
+            trie.AddElement("doing");
+            trie.AddElement("milk");
+            trie.AddElement("do");
+            trie.AddElement("car");
+            return trie.Contains("do");
+      }
+
+      private static bool RemoveTest()
+      {
+            Trie trie = new();
+            trie.AddElement("dog");
+            trie.AddElement("doing");
+            trie.AddElement("milk");
+            trie.AddElement("do");
+            trie.AddElement("car");
+            return trie.Remove("do");
+      }
+
+      private static bool HowManyStartsWithPrefixTest()
+      {
+            Trie trie = new();
+            trie.AddElement("dog");
+            trie.AddElement("doing");
+            trie.AddElement("milk");
+            trie.AddElement("do");
+            trie.AddElement("car");
+            return trie.HowManyStartsWithPrefix("do") == 3;
+      }
+}
