39d50c9ea0048532742199ae34984225e51c910f
marina-popova11 <181482786+marina-popova11@users.noreply.github.com>
2025-03-15 19:48:41 +0300

    Added several tests for Compress

--- homework3/LZW.Algorithm/LZW.Algorithm.Tests/TestCompress.cs
+++ homework3/LZW.Algorithm/LZW.Algorithm.Tests/TestCompress.cs
@@ -1,12 +1,52 @@
 ﻿using LZW.Algorithm;
+using NUnit.Framework.Constraints;
 
 namespace LZW.Algorithm.Tests;
 
 public class Tests
 {
-    [Test]
-    public void Test_CompressData()
+    private LZWCompression lzw = new(); 
+    [SetUp]
+    public void SetUp()
     {
         var lzw = new LZWCompression();
     }
+    [Test]
+    public void Test_CompressEmptySet()
+    {
+        var filePath = Path.GetTempFileName();
+        File.WriteAllBytes(filePath, Array.Empty<byte>());
+        (var compressed, var cRatio) = lzw.Compress(filePath);
+        Assert.That(compressed, Is.Empty);
+        File.Delete(filePath);
+    }
+
+    [Test]
+    public void Test_CompressTwoElement()
+    {
+        byte[] array = [99, 3];
+        var filePath = Path.GetTempFileName();
+        File.WriteAllBytes(filePath, array);
+        (var compressed, var cRatio) = lzw.Compress(filePath);
+        Assert.That(compressed, Is.Not.Empty);
+        File.Delete(filePath);
+    }
+
+    [Test]
+    public void Test_CompressTenElements()
+    {
+        var filePath = Path.GetTempFileName();
+        File.WriteAllText(filePath, "AAAAA");
+        (var compressed, var cRatio) = lzw.Compress(filePath);
+        Assert.That(compressed, Is.Not.Empty);
+        Assert.That(cRatio, Is.GreaterThan(0));
+        File.Delete(filePath);   
+    }
+    
+    [Test]
+    public void Compress_InvalidFilePathThrowsException()
+        {
+            var invalidFilePath = "invalid_path.txt";
+            Assert.Throws<FileNotFoundException>(() => lzw.Compress(invalidFilePath));
+        }
 }
