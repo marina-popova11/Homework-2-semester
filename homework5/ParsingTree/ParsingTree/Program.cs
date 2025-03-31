e688edc8c6708bcc39bf0062adf3b4835d43c9b2
marina-popova11 <181482786+marina-popova11@users.noreply.github.com>
2025-03-31 22:12:36 +0300

    Added the nodes in Tree

--- homework5/ParsingTree/ParsingTree/Program.cs
+++ homework5/ParsingTree/ParsingTree/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using ParsingTree;
+
+if (args.Length < 1)
+{
+      System.Console.WriteLine("Enter the filename!");
+}
+
+string input = args[0];
+if (string.IsNullOrWhiteSpace(input))
+{
+      System.Console.WriteLine("File path cannot be null.");
+      return;
+}
+
+if (!File.Exists(input))
+{
+      System.Console.WriteLine("File not exists, try again.");
+      return;
+}
+
+if (new FileInfo(input).Length == 0)
+{
+      System.Console.WriteLine("File is empty, try again.");
+      return;
+}
+
+string inputData = File.ReadAllText(input).Trim();
+Node root = GetParsingTree(inputData);
+var tree = new Tree(root);
+
+Node GetParsingTree(string data)
+{
+      data = data.Trim('(', ')');
+      var parts = data.Split(' ');
+
+}
