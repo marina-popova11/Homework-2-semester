4e60e3d02a0e59da3b7cbd6620941df4f29020ce
marina-popova11 <181482786+marina-popova11@users.noreply.github.com>
2025-05-22 14:19:16 +0300

    Create project

---
+++ FinalTest/FinalTest/FinalTest.Tests/Tests.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="Tests.cs" company="Marina-Popova">
+// Copyright (c) Marina-Popova. All rights reserved.
+// </copyright>
+
+namespace FinaTest.Tests;
+
+/// <summary>
+/// Tests for.
+/// </summary>
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ FinalTest/FinalTest/FinalTest/Program.cs
@@ -0,0 +1,5 @@
+﻿// <copyright file="Program.cs" company="Marina-Popova">
+// Copyright (c) Marina-Popova. All rights reserved.
+// </copyright>
+
+Console.WriteLine("Hello, World!");
